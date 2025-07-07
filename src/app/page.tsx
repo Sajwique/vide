5b9f4ecb6411fa1cb1bd413a32609f62f5de8d73
Sajwique <sajwique@gmail.com>
2025-07-07 10:02:24 +0530

    database added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const user = await prisma.user.findMany();
   return (
     <div className="">
       <Button>Click me</Button>
